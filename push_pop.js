b13d15c3132787b53d677c03825fb81e9b207fec
Johan Lidberg <johan.lidberg@skola.botkyrka.se>
2020-12-07 15:55:58 +0100

    Push_pop.js

---
+++ push_pop.js
@@ -0,0 +1,16 @@
+//push_pop.js//
+
+//pop är en funktion som gör att du kan ta bort det sista elementet i din array. Exempel
+let name = ["Björn", "Einar", "Peter"]
+
+name.pop()
+//den kommer då loga ut endast Björn och Einar.
+
+//Push är när du vill lägga till ett element.Exempel
+
+let name = ["Peter"]
+
+name.push(Einar)
+
+//Funkar även att ta med flera elemnet samtidigt//
+name.push("Einar", "Björn")
